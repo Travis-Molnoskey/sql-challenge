265ca258cf762ac616a6107a2ac2c076193ff315
Travis Molnoskey <t.molnoskey@gmail.com>
2020-10-20 20:12:36 -0700

    adding SQL schema file for creating tables

---
+++ EmployeeSQL/sql_schemas.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS titles CASCADE;
+CREATE TABLE titles(
+	title_id INTEGER PRIMARY KEY,
+	title VARCHAR
+);
+
+DROP TABLE IF EXISTS employees CASCADE; 
+CREATE TABLE employees(
+	emp_no INTEGER PRIMARY KEY,
+	emp_title_id INTEGER,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	sex VARCHAR,
+	hire_date DATE
+);
+
+DROP TABLE IF EXISTS departments CASCADE;
+CREATE TABLE departments(
+	dept_no INTEGER PRIMARY KEY,
+	dept_name VARCHAR
+);
+
+DROP TABLE IF EXISTS dept_emp CASCADE;
+CREATE TABLE dept_emp(
+	emp_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	dept_no INTEGER,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+DROP TABLE IF EXISTS salaries CASCADE;
+CREATE TABLE salaries(
+	emp_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INTEGER
+);
+
+DROP TABLE IF EXISTS dept_manager CASCADE;
+CREATE TABLE dept_manager(
+	dept_no INTEGER,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
